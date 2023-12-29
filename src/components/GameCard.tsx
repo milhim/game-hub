7f10898f8987fb4984ecd2d80f2e38bda82ea26f
Molhem Molhem <u13543@isoint.net>
2023-12-29 20:24:21 +0300

    adding badge score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
     game: Game;
@@ -12,10 +13,13 @@ const GameCard = ({ game }: Props) => {
             <Image src={game.background_image}></Image>
             <CardBody>
                 <Heading fontSize={"2xl"}>{game.name}</Heading>
+                <HStack justifyContent="space-between">
+                    <PlatformIconList
+                        platforms={game.platforms.map((p) => p.platform)}
+                    ></PlatformIconList>
+                    <CriticScore score={game.metacritic}></CriticScore>
+                </HStack>
             </CardBody>
-            <PlatformIconList
-                platforms={game.platforms.map((p) => p.platform)}
-            ></PlatformIconList>
         </Card>
     );
 };
