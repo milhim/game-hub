82ee80febbcc5c6eb4e17b3dfb546dd8b7e547d5
Molhem Molhem <u13543@isoint.net>
2023-12-29 20:47:56 +0300

    show loading placeholder

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,12 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-    const { games, errors } = useGames();
+    const { games, errors, isLoading } = useGames();
+    const skeletons = [1, 2, 3, 4, 5, 6];
+
     return (
         <>
             {errors && <Text>{errors}</Text>}
@@ -12,6 +15,10 @@ const GameGrid = () => {
                 spacing={10}
                 padding={12}
             >
+                {isLoading &&
+                    skeletons.map((skeleton) => (
+                        <GameCardSkeleton key={skeleton} />
+                    ))}
                 {games.map((game) => (
                     <GameCard key={game.id} game={game} />
                 ))}
