7cf87ccf4a509fd88dc4cf4816cbc72be4f401c4
Molhem Molhem <u13543@isoint.net>
2024-01-06 20:26:46 +0300

    filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,23 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenre from "../hooks/useGenre";
+import {
+    Button,
+    HStack,
+    Image,
+    List,
+    ListItem,
+    Spinner,
+    Text,
+} from "@chakra-ui/react";
+import useGenre, { Genre } from "../hooks/useGenre";
 import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
+interface Props {
+    onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
     const { data, isLoading, errors } = useGenre();
 
-    if (errors) return null;
+    // if (errors) return null;
     if (isLoading) return <Spinner />;
     return (
         <List>
@@ -17,7 +29,15 @@ const GenreList = () => {
                             borderRadius={8}
                             src={getCroppedImageUrl(genre.image_background)}
                         />
-                        <Text fontSize="large">{genre.name}</Text>
+                        <Button
+                            onClick={() => {
+                                onSelectGenre(genre);
+                            }}
+                            fontSize="large"
+                            variant="link"
+                        >
+                            {genre.name}
+                        </Button>
                     </HStack>
                 </ListItem>
             ))}
