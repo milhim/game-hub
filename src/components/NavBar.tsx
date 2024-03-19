9ec470f984baf56799e44d85d22fc94131ad6741
Milhim Milhim <milhim.a.milhim@gmail.com>
2024-03-19 23:09:35 +0300

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 const NavBar = () => {
-    return (
-        <HStack justifyContent="space-between">
-            <Image src={logo} boxSize="50px" />
-            <ColorModeSwitch></ColorModeSwitch>
-        </HStack>
-    );
+  return (
+    <HStack padding={10}>
+      <Image src={logo} boxSize="50px" />
+      <SearchInput />
+      <ColorModeSwitch></ColorModeSwitch>
+    </HStack>
+  );
 };
 
 export default NavBar;
